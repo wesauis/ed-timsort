b09c798a76eb3cafd979714961e3d8a2f9f52568
wesauis <54046750+wesauis@users.noreply.github.com>
2021-04-22 14:33:40 -0300

    vscode python test debugger

--- __tests__/test_binary_insertion_sort.py
+++ __tests__/test_binary_insertion_sort.py
@@ -1,35 +1,29 @@
 from timsort import binary_insertion_sort
 from utils.asserts import assert_has_all, assert_sorted
 from utils.randomplus import randarray
-from nose2.tools import params
+from random import randint
 
 
-SIZE = 63
+def test_is_sorting():
+    for to_sort in map(
+        lambda _: randarray(randint(48, 256)),
+        [None] * randint(3, 12)
+    ):
+        sorted = binary_insertion_sort(to_sort, 0, len(to_sort), 0)
+
+        assert_sorted(sorted)
+        assert_has_all(to_sort, sorted)
 
 
-@params(
-    randarray(SIZE),
-    randarray(SIZE),
-    randarray(SIZE),
-    randarray(SIZE),
-    randarray(SIZE),
-    randarray(SIZE),
-)
-def test_insertion_sort(to_sort: list[int]):
-    """Tests if the array is been sorted"""
+def test_works_with_small_arrays():
+    for to_sort in (
+        randarray(0),
+        randarray(1),
+        randarray(2),
+        randarray(3),
+        randarray(4)
+    ):
+        sorted = binary_insertion_sort(to_sort, 0, len(to_sort), 0)
 
-    sorted = binary_insertion_sort(to_sort, 0, len(to_sort), 0)
-
-    assert_sorted(sorted)
-    assert_has_all(to_sort, sorted)
-
-
-@params(
-    randarray(0),
-    randarray(1),
-    randarray(2),
-    randarray(3),
-    randarray(4),
-)
-def test_works_with_small_arrays(to_sort: list[int]):
-    test_insertion_sort(to_sort)
+        assert_sorted(sorted)
+        assert_has_all(to_sort, sorted)
